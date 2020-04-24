ecdff9cd8426c27cdcecc4f56839837be8a2f367
Jonah Mallard <benjaminjmallard@gmail.com>
2020-04-24 22:49:03 +0000

    Automatically backed up by Learn

---
+++ prime.rb
@@ -0,0 +1,8 @@
+def is_prime?(num)
+  return false if !num.integer?
+   return false if num < 2 
+    return true if num == 2 
+  (2..num-1).each {|int| return false if num % int == 0}
+  true
+end 
+
